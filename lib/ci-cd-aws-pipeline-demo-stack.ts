2aeb08c6720663a77540e4c1bb73767cdb30c3df
sammykirigha <dkirigha18@gmail.com>
2023-02-16 10:49:30 +0300

    added a new cicd project

--- lib/ci-cd-aws-pipeline-demo-stack.ts
+++ lib/ci-cd-aws-pipeline-demo-stack.ts
@@ -1,16 +1,19 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
+import {CodePipeline, CodePipelineSource, ShellStep} from "aws-cdk-lib/pipelines"
+import {ManualApprovalStep} from "aws-cdk-lib/pipelines"
 // import * as sqs from 'aws-cdk-lib/aws-sqs';
 
 export class CiCdAwsPipelineDemoStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
-
-    // The code that defines your stack goes here
-
-    // example resource
-    // const queue = new sqs.Queue(this, 'CiCdAwsPipelineDemoQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+   
+    new CodePipeline(this, 'Pipeline', {
+      pipelineName: "TestPipeline",
+      synth: new ShellStep('Synth', {
+        input: CodePipelineSource.gitHub('sammykirigha/ci-cd-aws-pipeline-demo', 'main'),
+        commands: ['npm ci', 'npm run build', 'npx cdk synth']
+      })
+    })
   }
 }
